2763fd8ac7f4d3ebd7ad46cf9815e5c9b4d6d60f
Pedro Juan Royo <pedro.juan.royo@gmail.com>
2026-08-10 17:18:38 +0200

    add __version__

--- src/marimol/__init__.py
+++ src/marimol/__init__.py
@@ -1,7 +1,12 @@
+import importlib.metadata
+
 from .utils import get_color, get_radius, resolve_color
 from .viewer import MoleculeViewerWidget, view_molecule
 
+__version__ = importlib.metadata.version("marimol")
+
 __all__ = [
+    "__version__",
     "view_molecule",
     "MoleculeViewerWidget",
     "get_color",
